b3fc82f8c19e39ae55650b8ec03652c9408699f1
amenk21 <amenallah.kthiri@esprit.tn>
2024-07-15 01:30:22 +0100

    .

--- crud_spring/crud_spring/src/main/java/com/coding/crud_spring/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coding.crud_spring.repository;
-
-import com.coding.crud_spring.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
-}
--- crud_spring/crud_spring/src/main/java/com/coding/crud_spring/repository/MissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coding.crud_spring.repository;
-
-import com.coding.crud_spring.entity.Mission;
-import com.coding.crud_spring.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface MissionRepository extends JpaRepository<Mission, Long> {
-}
